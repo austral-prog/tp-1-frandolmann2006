a0121db54a1feff261fa382888313d07f0e11e8b
frandolmann2006 <frankdolmann@gmail.com>
2026-03-10 14:53:54 -0300

    Update exercise_age.py

--- exercise_age.py
+++ exercise_age.py
@@ -9,7 +9,7 @@ def age():
     4. La edad en minutos (1 hora = 60 minutos)
     """
     edad_anos = 25
-    print (12*25)
-    print (365*25)
-    print (25*24*365)
-    print (60*24*365*25)
+    print (12*edad_anos)
+    print (365*edad_anos)
+    print (edad_anos*24*365)
+    print (60*24*365*edad_anos)
