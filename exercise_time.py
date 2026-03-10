a992ff677c84382e37d4be6264db1d5e920d9846
frandolmann2006 <frankdolmann@gmail.com>
2026-03-10 19:28:51 -0300

    Update exercise_time.py

--- exercise_time.py
+++ exercise_time.py
@@ -8,3 +8,8 @@ def time():
     3. Segundos restantes
     """
     total_segundos = 3665
+    hora_completa = 3600
+    minutos= 60
+    print(total_segundos//60//60)
+    print((total_segundos//hora_completa)%minutos)
+    print(total_segundos-hora_completa-minutos)
